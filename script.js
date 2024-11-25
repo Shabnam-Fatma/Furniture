719a1fa36508f4a16a22c4ef943f804b846bbb44
Shabnam-Fatma <144026012+Shabnam-Fatma@users.noreply.github.com>
2024-11-25 18:39:48 +0530

    Add files via upload
    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
+window.addEventListener('scroll', function () {
+    const nav = document.querySelector('nav');
+    if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+    } else {
+        nav.classList.remove('scrolled');
+    }
+});
+
+
